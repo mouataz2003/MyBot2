8c5fcf82404eed01a4e24c792d4a609b3349eb94
mouataz2003 <mouataz_bouazaoui@outlook.com>
2018-04-29 12:54:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 const config = require('./config.json')
 const { prefix, token } = require('./config.json')
-const YoutubeStream = require('ytdl-core')
 
 
 
